5ec5ad89eb2dd8fb044c5c3d75c824f50d9e7ba6
enz <enzo_naguiat@yahoo.com>
2019-06-15 22:25:29 +0900

    implement simple validations to, and, display error messages in the add car form

--- src/containers/car_new.jsx
+++ src/containers/car_new.jsx
@@ -5,11 +5,22 @@ import { connect } from 'react-redux';
 import { addCar } from '../actions/index';
 
 class CarNew extends Component {
-  renderInput({ input, label}){
+  renderError = ({touched, error}) => {
+    if (touched && error) {
+      return (
+        <div>
+          <div>{error}</div>
+        </div>
+      )
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
     return(
       <div>
         <label>{label}</label>
         <input {...input}></input>
+        {this.renderError(meta)}
       </div>
     )
   }
@@ -49,6 +60,27 @@ class CarNew extends Component {
   }
 }
 
-export default reduxForm({ form: 'newCarForm' })(
+const validate = (formValues) => {
+  let errors = {};
+  if (!formValues.brand) {
+    errors.brand = "You must enter the brand of the car";
+  }
+
+  if (!formValues.model) {
+    errors.model = "You must enter the model of the car";
+  }
+
+  if (!formValues.plate) {
+    errors.plate = "You must enter the plate of the car";
+  }
+
+  if (!formValues.owner) {
+    errors.owner = "You must enter the name of the owner of the car";
+  }
+
+  return errors;
+}
+
+export default reduxForm({ form: 'newCarForm', validate })(
   connect(null, { addCar })(CarNew)
 );
